33f4e2bedcf51d59124979ec1d182bc14c2643d6
ajsiddiqui07 <arbazsiddiqui769@gmail.com>
2026-06-29 10:39:44 +0530

    js

---
+++ ASSIGNMENT/Reactjs/Sesstion-1/myreactapp/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Component/Home';
+import TrendingSongs from './Component/TrendingSongs';
+
+
+function App() {
+  return (
+    <>
+    <h1>Welcome to My React Zomato App</h1>
+    <Home/>
+    <TrendingSongs/>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ ASSIGNMENT/Reactjs/Sesstion-1/myreactapp/src/Component/TrendingSongs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TrendingSongs() {
+  return (
+    <h2>Trending on Spotify: Calm Down</h2>
+  )
+}
+
+export default TrendingSongs
